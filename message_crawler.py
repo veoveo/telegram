46528aee7e67e5f80ecea521a5ac7081b85825b3
Phan Thế Minh Châu <84219414+veoveo@users.noreply.github.com>
2022-12-08 18:53:48 +0700

    Add files via upload

--- message_crawler.py
+++ message_crawler.py
@@ -2,6 +2,8 @@ from telethon import TelegramClient, sync
 import telethon
 import os
 import time
+import image_upload
+import re
 
 api_id = 29461139
 api_hash = '61d8d5fbfd1bc9afeeaaf9db1b4f64ed'
@@ -9,39 +11,28 @@ session_name = 'nghienshopeelazada'
 client = TelegramClient(session_name, api_id, api_hash).start()
 
 def fetch_one_message(msg):
-
-    if msg.media and isinstance(msg.media, telethon.tl.types.MessageMediaPhoto):
+    if msg.message.count("://shope.ee/") !=0 and msg.media and isinstance(msg.media, telethon.tl.types.MessageMediaPhoto):
         # Download image
-        media_path = msg.download_media('media/group_{}/{}_{}_{}'.format(msg.chat.id, msg.chat.id, msg.from_id, msg.id))
-        media_file = os.path.basename(media_path)
-    elif msg.media and isinstance(msg.media, telethon.tl.types.MessageMediaDocument) \
-            and msg.media.document.mime_type in ['audio/ogg']:
-        # Download voice
-        media_path = msg.download_media('media/group_{}/{}_{}_{}'.format(msg.chat.id, msg.chat.id, msg.from_id, msg.id))
-        media_file = os.path.basename(media_path)
+        media_path = msg.download_media('media/{}_{}_{}'.format(msg.chat.id, msg.from_id, msg.id))
+        path_file = os.path.basename(media_path)
+        media_file = image_upload.uploadimg("/content/media/"+path_file)
     else:
         media_file = None
         if not msg.message:
             return
+    
+    mess = msg.message
+    link = re.findall(r'(https?://shope.ee[^\s]+)', mess)
+    for i in link:
+      #sửa ở đây
+      mess.replace(i,móihdjshdhsj)
 
     item = {
-        "message_id": msg.id,  # Message id of current chat
-        "chat_id": msg.chat.id,  # ID of current chat
-        ## "is_out": msg.out,
-        # Whether the message is outgoing (i.e. you sent it from another session) or incoming (i.e. someone else sent it).
-        ## "is_mentioned": msg.mentioned,
-        # Whether you were mentioned in this message or not. Note that replies to your own messages also count as mentions
         "message": msg.message,  # message content
-        "date": msg.date,
-        "from_id": msg.from_id,  # The ID of the user who sent this message
-        "is_reply": msg.is_reply,  # True if the message is a reply to some other
-        "reply_to_msg_id": msg.reply_to_msg_id,  # The ID to which this message is replying to, if any
-        "is_channel": msg.is_channel,
-        "is_group": msg.is_group,
         "media_file": media_file
     }
 
-    print(msg.message)
+    print(item)
 
 def fetch_all_group_message(client):
     dialog_list = client.get_dialogs()
