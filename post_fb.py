af95452eb8e4752793602ed3ea58294534c90ecf
Phan Thế Minh Châu <84219414+veoveo@users.noreply.github.com>
2022-12-08 20:25:41 +0700

    Add files via upload

--- post_fb.py
+++ post_fb.py
@@ -2,9 +2,11 @@ import facebook
 
 def fb_post(msg,img):
   groups=['180249687420161']
-  token='EAAJkWb9NY98BANeZCAhQxsU1bKsVt1sJFQr71FsL9V0iMEYBrU2vgExTedN24ZBNBopOZAWN1V6GXxAZCOGcuhnIjsUGeXG8a7nGOiz8iZAtpcJZAtnRcZATBAzWAuvooMqrvg8jV36nPepkJTsXJDwao8X5M9RfZC8LvJdCzwqxoS4ZAEUKja58OwwCCQfkpqq8ZD'
+  token1='EAAJkWb9NY98BAKVsA3tSG5vqcdDWm9h6TDdyTUWS4KesHiKtouBtDFgz0YcfogsbvenGP'
+  token2='7zxmzyWPArsfw9HtWGQNG4k1ugZBsRZCW0TRUKvDbogiL2imSn31V84TEZA9INQDJzNPZBGTBrgXY'
+  token3='48gm8CRWkRInkijyEc2oagOm8oGWhy3btxYfxaEfwIWx0ZD'
   try:
-    graph = facebook.GraphAPI(access_token=token)
+    graph = facebook.GraphAPI(access_token=token1+token2+token3)
     for group in groups:
       x = graph.put_object(group, 'photos', message=msg,url=img)
       print(x)
